bcc178cf0145115b6fb7d107ad54bfe14744d313
mzain4321 <168978177+mzain4321@users.noreply.github.com>
2024-08-07 14:56:39 +0500

    Add files via upload

---
+++ HTML-CSS-JS/image gallery/gal.js
@@ -0,0 +1,27 @@
+let slideIndex = 1;
+function openLightbox() {
+  document.getElementById("lightbox").style.display = "block";
+  showSlides(slideIndex);
+}
+function closeLightbox() {
+  document.getElementById("lightbox").style.display = "none";
+}
+function changeSlide(n) {
+  showSlides((slideIndex += n));
+}
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+function showSlides(n) {
+  const slides = document.getElementsByClassName("slides");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex - 1].style.display = "block";
+}
